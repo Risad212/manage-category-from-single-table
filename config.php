c40947d6a9bf8db534c31e31c183e60e11d2f549
Risad212 <hafez.risad@gmail.com>
2025-01-30 21:36:44 +0600

    solve env problem

--- config.php
+++ config.php
@@ -1,18 +1,13 @@
 <?php
-
 require 'vendor/autoload.php'; // Load Composer's autoloader
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__); // Point to your project folder
 $dotenv->load(); // Load .env
 
 // Access environment variables
-$dbhost = getenv();
-// $db = $_ENV('DB_NAME');
-// $user = $_ENV('DB_USER');
-// $pass = $_ENV('DB_PASS');
+$dbhost = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$dbuser = $_ENV['DB_USER'];
+$dbpass = $_ENV['DB_PASS'];
 
 
-// Access environment variables
-
-
-var_dump($dbhost);  // Shows all environment variables
--- db.php
+++ db.php
@@ -2,13 +2,13 @@
 require_once('config.php');
 
 // Create connection
-// $conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 // Query to select all data from the 'menu_items' table
-// $query = "SELECT * FROM `location` WHERE parent_id IS NULL";
-// $result = $conn->query($query);
+$query = "SELECT * FROM `location` WHERE parent_id IS NULL";
+$result = $conn->query($query);
